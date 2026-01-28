036d7e575bc78c654a0d498aa0659ccb82330f80
HarneyHong <hongzhaoyan@ruc.edu.cn>
2026-01-28 20:29:14 +0800

    Add RCRank as submodule and remove test_sql_gen and tests directories

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pathlib import Path
-
-# Get the project root directory
-root_dir = Path(__file__).parent.parent
-
-# Add the project root directory to Python path
-sys.path.append(str(root_dir))
